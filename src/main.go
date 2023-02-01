171b0f6fe0df338d1250b92282797dca72471ce4
wenxuan <1595159972@qq.com>
2023-02-01 23:53:33 +0800

    V1.0
    初始化项目

---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"simpledouyin/src/dao"
+)
+
+func main() {
+	err := dao.InitMySql()
+	if err != nil {
+		panic(err)
+	}
+	//程序退出关闭数据库连接
+	defer dao.Close()
+	//绑定模型
+	dao.SqlSession.AutoMigrate(&entity.User{})
+	//注册路由
+
+	r := gin.Default()
+	initRouter(r)
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	//10.0.2.2是一个特殊IP，在模拟器上用10.0.2.2就是访问你的电脑本机
+}
