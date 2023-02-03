efc2d90365df8778fd20f2dc24b89fb611024106
wenxuan <1595159972@qq.com>
2023-02-03 16:22:45 +0800

    修改

--- src/common/errors.go
+++ src/common/errors.go
@@ -9,5 +9,5 @@ var (
 	Passwordtoolong   = errors.New("密码过长")
 	UserNameNotUnique = errors.New("用户名重复")
 	PasswordWrong     = errors.New("密码错误")
-	UserNoexist       = errors.New("用户不存在")
+	UserNoexist       = errors.New("该用户不存在")
 )
