88a83dcb808371acbef9973c25c4ca8c1477f697
wenxuan <1595159972@qq.com>
2023-02-07 21:03:00 +0800

    所有需要中间件的都加上

--- src/router.go
+++ src/router.go
@@ -23,14 +23,14 @@ func initRouter(r *gin.Engine) {
 	// extra apis - I
 	apiRouter.POST("/favorite/action/", middleware.JwtHandler(), controller.FavoriteAction)
 	apiRouter.GET("/favorite/list/", middleware.JwtHandler(), controller.FavoriteList)
-	//apiRouter.POST("/comment/action/", controller.CommentAction)
-	//apiRouter.GET("/comment/list/", controller.CommentList)
+	//apiRouter.POST("/comment/action/", middleware.JwtHandler(), controller.CommentAction)
+	//apiRouter.GET("/comment/list/", middleware.JwtHandler(), controller.CommentList)
 
 	// extra apis - II
 	apiRouter.POST("/relation/action/", middleware.JwtHandler(), controller.RelationAction)
-	//apiRouter.GET("/relation/follow/list/", controller.FollowList)
-	//apiRouter.GET("/relation/follower/list/", controller.FollowerList)
-	//apiRouter.GET("/relation/friend/list/", controller.FriendList)
-	//apiRouter.GET("/message/chat/", controller.MessageChat)
-	//apiRouter.POST("/message/action/", controller.MessageAction)
+	//apiRouter.GET("/relation/follow/list/", middleware.JwtHandler(), controller.FollowList)
+	//apiRouter.GET("/relation/follower/list/", middleware.JwtHandler(), controller.FollowerList)
+	//apiRouter.GET("/relation/friend/list/", middleware.JwtHandler(), controller.FriendList)
+	//apiRouter.GET("/message/chat/", middleware.JwtHandler(), controller.MessageChat)
+	//apiRouter.POST("/message/action/", middleware.JwtHandler(), controller.MessageAction)
 }
